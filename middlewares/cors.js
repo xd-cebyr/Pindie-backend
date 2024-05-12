0286e977bb945f803a042ee886952435c39acbf5
bestwizzar. <jiperschristmas@gmail.com>
2024-05-12 23:28:31 +0700

    d

---
+++ middlewares/cors.js
@@ -0,0 +1,18 @@
+const allowedCors = [
+  "https://practicum.yandex.ru",
+  "https://students-projects.ru",
+  "localhost:3011",
+  "localhost:3000",
+  "localhost:3001",
+];
+
+function cors(req, res, next) {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  next();
+}
+
+module.exports = cors;
