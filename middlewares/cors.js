8181b9051ca9db6c67f5fc13a323f1a1ef95d44a
bestwizzar. <jiperschristmas@gmail.com>
2024-05-20 00:22:33 +0700

    m

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,17 +1,16 @@
-const allowedCors = [
-  "https://practicum.yandex.ru",
-  "https://students-projects.ru",
-  "localhost:3011",
-  "localhost:3000",
-  "localhost:3001",
-];
+const allowedCors = ["https://pindie-frontend.nomoredomainswork.ru"];
 
 function cors(req, res, next) {
   const { origin } = req.headers;
+
   if (allowedCors.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
   }
-
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+  );
   next();
 }
 
